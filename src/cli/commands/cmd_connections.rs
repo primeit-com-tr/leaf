c3e61fb168817fd89de84757413a3fde8592e408
Ceyhun <ceyhun.kerti@gmail.com>
2025-11-24 23:10:33 +0300

    rm p

--- src/cli/commands/cmd_connections.rs
+++ src/cli/commands/cmd_connections.rs
@@ -132,7 +132,7 @@ pub async fn prune(yes: &bool, ctx: &Context<'_>) {
             .unwrap_or(false);
 
     if !proceed {
-        println!("✅ Aborted");p
+        println!("✅ Aborted");
         return;
     }
 
